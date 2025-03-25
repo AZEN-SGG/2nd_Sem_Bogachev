f44d7a9a6b1f3f89563788a944259c5e822f0370
AZEN-SGG <krivoruchenko.sergei@gmail.com>
2025-03-25 23:24:57 +0300

    chore: delete obsolete Matvei project files

--- 2025.03.28/Matvei/array.h
+++
@@ -1,5 +0,0 @@
-io_status read_matrix(double*, int, int, const char*);
-void print_matrix(const double*, int, int, int);
-void init_matrix(double*, int, int, int);
-double f(int, int, int, int, int);
-void init_vector(double*, double*, int);
--- 2025.03.28/Matvei/io_status.h
+++
@@ -1,6 +0,0 @@
-typedef enum io_status_
-  {
-  SUCCESS,
-  ERROR_OPEN,
-  ERROR_READ,
-  } io_status;
