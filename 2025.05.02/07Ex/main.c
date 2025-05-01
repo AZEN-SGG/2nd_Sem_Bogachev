7144337f0be47dc4aeaf48eb0979df5474a5a5ed
AZEN-SGG <74971141+AZEN-SGG@users.noreply.github.com>
2025-05-01 19:05:23 +0300

    Task 7 is done

---
+++ 2025.05.02/07Ex/main.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <time.h>
+#include <math.h>
+
+#include "init_f.h"
+#include "solve.h"
+
+/* ./a.out x_0 eps M k */
+int main(int argc, char *argv[])
+{
+	double t, x_0, eps, x = 0;
+	int m, k, cl, it, task = 7;
+	
+	double (*f) (double);
+	double (*f_lst[]) (double) = {f0, f1, f2, f3, f4, f5, f6, sin}; // TODO: Remove sin
+	int len_f = sizeof(f_lst) / sizeof(f_lst[0]);
+
+	if (
+			!((argc == 5) && 
+			sscanf(argv[1], "%lf", &x_0) == 1 &&
+			(sscanf(argv[2], "%lf", &eps) == 1 && (eps >= 0)) &&
+			((sscanf(argv[3], "%d", &m) == 1) && (m > 0)) &&
+			((sscanf(argv[4], "%d", &k) == 1) && ((0 <= k) && (k <= len_f))))
+	) {
+		fprintf(stderr, "Usage: %s x_0 eps M k\n", argv[0]);
+		return -1;
+	}
+
+	f = f_lst[k];
+
+	t = clock();
+	it = t7_solve(f, x_0, eps, m, &x);
+	t = (clock() - t) / CLOCKS_PER_SEC;
+		
+	cl = get_call_count();
+
+	if (it >= m)
+	{
+		fprintf(stdout, "%s : Task = %d NOT FOUND Count = %d T = %.2f\n", argv[0], task, cl, t);
+		return -2;
+	} else
+	{
+		printf("%s : Task = %d X = %e Res = %e Its = %d Count = %d T = %.2f\n", argv[0], task, x, f(x), it, cl, t);
+		return 0;
+	}
+}
+
---
+++ 2025.05.02/07Ex/solve.c
@@ -0,0 +1,33 @@
+#include "solve.h"
+
+#include <math.h>
+#include <float.h>
+
+int t7_solve (
+		double (*f) (double), 
+		double x_0, double eps, 
+		int m, double *x
+) {
+	int it = 0;
+	double y = f(x_0);
+
+	if (fabs(y - x_0) - eps < DBL_EPSILON)
+	{
+		*x = x_0;
+		return 1;
+	}
+
+	for (it = 0; it < m; ++it)
+	{
+		x_0 = y;
+		y = f(x_0);
+		
+		if (fabs(y - x_0) - eps < DBL_EPSILON)
+			break;
+	}
+	
+	*x = x_0;
+
+	return it;
+}
+
