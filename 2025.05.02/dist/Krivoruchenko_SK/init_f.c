f140ae9722db535cca6fedff5d13921ef9e0606f
AZEN-SGG <krivoruchenko.sergei@gmail.com>
2025-05-11 15:17:58 +0300

    return init.f

--- 2025.05.02/dist/Krivoruchenko_SK/init_f.c
+++ 2025.05.02/dist/Krivoruchenko_SK/init_f.c
@@ -4,10 +4,21 @@
 
 
 static int cl = 0;
+static int cl_d = 0;
 
 int get_call_count (void) 
 {
 	return cl;
+}
+
+int get_call_function_count (void) 
+{
+	return cl_f;
+}
+
+int get_call_derivative_count (void)
+{
+	return cl_d;
 }
 
 double f0 (double x)
@@ -18,10 +29,25 @@ double f0 (double x)
 	return 1;
 }
 
+double d0 (double x)
+{
+	cl_d++;
+	(void)x;
+
+	return 0;
+}
+
 double f1 (double x)
 {
 	cl++;
 	return (x * 1e-100) - 1.0;
+}
+
+double d1 (double x)
+{
+	(void)x;
+	cl_d++;
+	return 1;
 }
 
 double f2 (double x)
@@ -29,6 +55,12 @@ double f2 (double x)
 	double x_2 = x * x;
 	cl++;
 	return 4 - x_2;
+}
+
+double d2 (double x)
+{
+	cl_d++;
+	return -2 * x;
 }
 
 double f3 (double x)
@@ -40,6 +72,13 @@ double f3 (double x)
 	return x_3 + 3 * x_2 + 16;
 }
 
+double d3 (double x)
+{
+	cl_d++;
+
+	return 3 * x * x + 6 * x;
+}
+
 double f4 (double x)
 {
 	double x_2 = x * x;
@@ -49,11 +88,28 @@ double f4 (double x)
 	return 3 - 2 * x_2 - x_4;
 }
 
+double d4 (double x)
+{
+	cl_d++;
+
+	return -4 * x - 4 * ((x * x) * x);
+}
+
 double f5 (double x)
 {
 	double sq_x = sqrt(fabs(x) + 1);
 	cl++;
 	return sq_x - 2;
+}
+
+double d5 (double x)
+{
+	cl_d++;
+	if (x < -DBL_EPSILON)
+		return (-1.0 / (sqrt(fabs(x) + 1))) * 0.5;
+	else if (x > DBL_EPSILON)
+		return (1.0 / (sqrt(fabs(x) + 1))) * 0.5;
+	return 0;
 }
 
 double f6 (double x)
@@ -64,3 +120,14 @@ double f6 (double x)
 	return sq - 2;
 }
 
+double d6 (double x)
+{
+	double sqrt_x = sqrt(fabs(x) + 1);
+	cl_d++;
+	
+	if (x < -DBL_EPSILON)
+		return (-1.0 / (sqrt(sqrt_x + 1) * sqrt_x)) * 0.25;
+	else if (x > DBL_EPSILON)
+		return (1.0 / (sqrt(sqrt_x + 1) * sqrt_x)) * 0.25;
+	return 0;
+}
