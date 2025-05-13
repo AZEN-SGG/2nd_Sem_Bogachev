17a04e4bee1c36d6f19935828f2f282bd4dc40e8
AZEN-SGG <rascatov.alex@gmail.com>
2025-05-13 20:06:25 +0300

    Start task 9

---
+++ 2025.05.09/09Ex/solve.c
@@ -0,0 +1,53 @@
+#include "solve.h"
+
+#include <math.h>
+#include <float.h>
+
+#define MAX_ITER 30
+
+int t9_solve (
+		double (*f) (double), 
+		double a, double b,
+		double eps, double *res
+) {
+	int it;
+	double integ_n;
+
+	double n = 2;
+	double h = (b - a) * 0.5;
+	double s1 = (f(a) + f(b)) * h/3;
+	double s2 = f(a + h) * h*4/3;
+
+	for (it = 1; it <= MAX_ITER; ++it)
+	{
+		double x = a + h*0.5;
+		double s2_2n = 0;
+
+		for (int i = 1; i < n; i++)
+		{
+			s2_2n += f(x);
+			x += h;
+		}
+
+		s2_2n *= h*2/3;
+
+		if (fabs(s2_2n - s1 * 0.5 - s2 * 0.75) < eps) {
+			integ_n = s1 + s2;
+			break;
+		}
+		
+		s1 = s1 * 0.5 + s2 * 0.25;
+		s2 = s2_2n;
+
+		h *= 0.5;
+		n *= 2;
+	}
+
+	if (it > MAX_ITER) 
+		return -1;
+	
+	*res = integ_n;
+
+	return n;
+}
+
---
+++ 2025.05.09/09Ex/solve.h
@@ -0,0 +1,10 @@
+#ifndef SOLVE_H
+#define SOLVE_H
+
+int t9_solve (
+		double (*f) (double), 
+		double a, double b,
+		double eps, double *res
+);
+
+#endif
