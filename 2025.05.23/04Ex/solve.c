6f21fa116f4bec322a183a66b53a5d71352b0324
AZEN-SGG <rascatov.alex@gmail.com>
2025-05-20 23:03:18 +0300

    Task 4 is done

--- 2025.05.23/04Ex/solve.c
+++ 2025.05.23/04Ex/solve.c
@@ -16,7 +16,13 @@ int t4_solve (node *head)
 		int last_cmp = strcmp(head->string, last->string);
 		
 		if (last_cmp == 0) {
-			local += (local > 0);
+			if (strict) {
+				if (maximum < count)
+					maximum = count;
+				count = 0;
+				local = 1;
+			} else 
+				local += (local > 0);
 		} else if (last_cmp < 0) {
 			if (strict) {
 				if (maximum < count)
@@ -39,32 +45,4 @@ int t4_solve (node *head)
 
 	return maximum;
 }
-/*
-	char *last = head->string;
-	int maximum = 0, count = 0, local = 0, strict = 0;
 
-	for (head = head->next; head; head = head->next)
-	{
-		int cmp = strcmp(head->string, last);
-		if (cmp > 0) {
-			count += local;
-			if (maximum < count)
-				maximum = count;
-			count = 0;
-			local = 1;
-			strict = 1;
-		} else if (cmp < 0) {
-			if (!strict)
-				count += local;
-			local = 1;
-			strict = 0;
-		} else {
-			if (local)
-				local++;
-		}
-		last = head->string;
-	}
-	
-	return maximum;
-}
-*/
